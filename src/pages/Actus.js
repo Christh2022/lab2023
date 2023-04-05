0c64d6d9f8e5bb382dc1fa44a7baf26ed1107780
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-04-05 15:46:38 +0200

    mise à jour de Actus

--- src/pages/Actus.js
+++ src/pages/Actus.js
@@ -3,11 +3,24 @@ import NavbarComponent from '../component/navbar/navbarComponent';
 import './Actus.css'
 import rectangle from '../assets/Rectangle 23.png'
 import Footer from '../component/footer/Footer';
-import img1 from '../assets/awrd pierre de marre 1.png'
+import img1 from '../assets/zyro-image.png'
+import img2 from '../assets/article 2.jpg'
 import {RxCross2} from 'react-icons/rx'
 
 const Actus = ({isActive, setIsActive}) => {
-    const [tab, setTab] = useState([img1, img1, img1, img1, img1, img1, img1])
+    const [tab, setTab] = useState([
+        {   
+            image: img1, 
+            titre: "Pierre de Maere et son équipe", 
+            description: "Dès son âge, Pierre de Maere compose ces chansons à l'aide de son frère Xavier de maere. En effet, ingénieur de son, il accompagne son petit frère pour composer ces chansons, c'est un peu le chef d'orchestre de l'album! . Donc, regarde - Moi est le fruit d'un travail entre deux frères qui aiment la musique et veulent partager leurs passion. En plus de son frère, on retrouve Gaspard Gomis qui est le batteur de Pierre depuis ces débuts et la pianiste Elodie charmansat. Ils ont une relation particulière avec l'artiste, en effet ce dont les deux colocataires de Pierre de Maere. On constate que Pierre de Maere est un artiste qui préfère être entouré de ces proches meme quand il s'agit du travail."
+        }, 
+        {
+            image: img2, 
+            titre: "ou trouvez la star belge", 
+            description: " pierre de Maere est l'interprète  du tube un jour, je marierai un ange, compte faire une tournée suite à la sortie de son nouvel album 'Regarde - Moi'. En effet, il suit le chemin de son artiste préféré Stramae puisqu'il compte se produire à  l'Olympia le 12 Mai 2023. Si vous n'habitez pas à Paris pas de panique ! il fait toute la tournée de Francde  en Plus de Bruxelles le 18 Mai 2023.  Pour soutenir la lutte contre le sida, Pierre de Maere rejoint l'affiche du festival Les solidays du 23 au 25 juin 2023 à l'Hippodrome de Paris - Longchamp. En plus de l'artiste belge, on retrouvera Angèle, Juliette Armanet, Bigflo & Oli, Jain. Pour plus d'information relative au festival, vous pouvez retrouver le lien ci dessous le lien de la billeterie : https://www.solidays.org"
+        }
+    ])
+    const [newObject, setNewObject] = useState({})
     const [email, setEmail] = useState("")
     const [actus, setActus] = useState(false)
     const handleSubmit = (e)=>{
@@ -32,17 +45,16 @@ const Actus = ({isActive, setIsActive}) => {
                 <div className='actus_article'>
                     <article>
                         {tab.map((value, index) =>
+                        <>
                         <div key={index}>
-                            <img src={value} alt="/" />
+                            <img src={value.image} alt="/" />
                             <span>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
+                               {value.titre}
                             </span>
-                            <button onClick={handleSeeActus}>lire plus</button>
-                        </div>)}
+                            <button onClick={()=>{
+                                handleSeeActus() 
+                                setNewObject({image: value.image, titre: value.titre, description: value.description})}}>lire plus</button>
+                        </div></>)}
                     </article>
                 </div>
                 <div className='newsletter actus'> 
@@ -62,25 +74,11 @@ const Actus = ({isActive, setIsActive}) => {
                 <div style={{width: '100%', height: '100vh', padding: '5rem 2rem'}}>
                     <div className='actus_wrapper_infos'>
                         <div>
-                            <img src={img1} alt="/" />
+                            <img src={newObject.image} alt="/" />
                         </div>
+                        <h2>{newObject.titre}</h2>
                         <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.... 
+                        {newObject.description}  
                         </p>
                     </div>
                     <span className='actus_wrapper_infos_close' onClick={handleSeeActus}>
